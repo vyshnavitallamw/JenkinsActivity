253557a341de8266dc926c9bd9979f3895c34d1e
vyshnavitallamw <166794340+vyshnavitallamw@users.noreply.github.com>
2024-05-10 15:34:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h2>Hello World- Docker flask deployment lab</h2><hr/>"
-    return "hey this is a new change i committed"
+    return "<h2>Hello World- Docker flask deployment lab- vyshnavi tallam</h2><hr/>"
 
 app.run(host="0.0.0.0",port=5000)
